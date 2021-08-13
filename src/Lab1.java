fd68c030d78c052232c7029321eb0b0161c31eb6
Yash Vishnoi <jivishnoi@gmail.com>
2021-08-13 16:23:34 +0530

    Test2

--- src/Lab1.java
+++ src/Lab1.java
@@ -4,6 +4,7 @@ public final class Lab1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Jenkins ");
+		System.out.println("Hello Goyal Saheb");
 	}
 
 }
